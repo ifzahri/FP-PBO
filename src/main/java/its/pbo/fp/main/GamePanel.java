ce78967b4557600b385f7ebbaa154f483a25d1b2
Iftala <circlebytes.iftala@gmail.com>
2023-11-05 11:33:16 +0700

    feat: add game loop and fps counter

--- src/main/java/its/pbo/fp/main/GamePanel.java
+++ src/main/java/its/pbo/fp/main/GamePanel.java
@@ -3,37 +3,61 @@ package its.pbo.fp.main;
 import its.pbo.fp.inputs.KeyboardInputs;
 import its.pbo.fp.inputs.MouseInputs;
 
-import java.awt.Graphics;
+import java.awt.*;
 import javax.swing.JPanel;
+import java.util.Random;
+
 public class GamePanel extends JPanel {
     private MouseInputs mouseInputs;
     private int xChanges = 200;
     private int yChanges = 100;
+    private int xDir = 1, yDir = 1;
+    private Color color = new Color(150, 20, 90);
+    private Random random;
     public GamePanel() {
+        random = new Random();
         mouseInputs = new MouseInputs(this);
         addKeyListener(new KeyboardInputs(this));
         addMouseListener(mouseInputs);
         addMouseMotionListener(mouseInputs);
     }
 
-    public void paintComponent(Graphics g) {
-        super.paintComponent(g);
-
-        g.fillRect(xChanges, yChanges, 100, 40);
-    }
-
     public void changeXMovement(int value) {
         this.xChanges += value;
-        repaint();
     }
     public void changeYMovement(int value) {
         this.yChanges += value;
-        repaint();
     }
-
     public void setRectPosition(int x, int y) {
         this.xChanges = x;
         this.yChanges = y;
-        repaint();
+    }
+    public void paintComponent(Graphics g) {
+        super.paintComponent(g);
+        updateRectangle();
+
+        g.setColor(color);
+        g.fillRect(xChanges, yChanges, 200, 50);
+    }
+
+    private void updateRectangle() {
+        xChanges += xDir;
+        if (xChanges > 400 || xChanges < 0) {
+            xDir *= -1;
+            color = getRndColor();
+        }
+        yChanges += yDir;
+        if (yChanges > 400 || yChanges < 0) {
+            yDir *= -1;
+            color = getRndColor();
+        }
+    }
+
+    private Color getRndColor() {
+        int r = random.nextInt(255);
+        int g = random.nextInt(255);
+        int b = random.nextInt(255);
+
+        return new Color(r, g, b);
     }
 }
--- src/main/java/its/pbo/fp/main/GameWindow.java
+++ src/main/java/its/pbo/fp/main/GameWindow.java
@@ -5,12 +5,13 @@ public class GameWindow {
     private JFrame jframe;
     public GameWindow(GamePanel gamePanel) {
         jframe = new JFrame("Tes");
-        Toolkit tk = Toolkit.getDefaultToolkit();
-        Dimension screenSize = tk.getScreenSize();
+//        Toolkit tk = Toolkit.getDefaultToolkit();
+//        Dimension screenSize = tk.getScreenSize();
 
         // Initialize Java Window
         jframe.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
-        jframe.setSize(screenSize.width, screenSize.height);
+//      jframe.setSize(screenSize.width, screenSize.height);
+        jframe.setSize(400, 400);
         jframe.add(gamePanel);
         jframe.setLocationRelativeTo(null);
         jframe.setVisible(true);
